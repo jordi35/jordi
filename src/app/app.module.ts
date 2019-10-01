510b88199c19e8ee55eeefc1960aa482c3f181fd
LAGUNAS ANTONIO REY DAVID <rey.lagunas@ine.mx>
2019-10-01 18:46:10 -0500

    primer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// rutas
+import {APP_ROUTING} from './app.routes';
+
+// servicios
+import {HeroesService} from './servicios/heroes.service';
+
+
+// componentes
+
+
+
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/shared/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
-  providers: [],
+
+  providers: [
+HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
